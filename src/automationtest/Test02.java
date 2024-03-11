4825c56db0697d095e7d9ddc56788957eb3287cc
DELL <karthikaeg5@gmail.com>
2024-03-11 16:14:04 +0530

    First Commit

---
+++ src/automationtest/Test02.java
@@ -0,0 +1,58 @@
+package automationtest;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test02 {
+	WebDriver driver;
+	@BeforeTest
+	public void setup()
+	{
+		driver= new ChromeDriver();
+	}
+	@BeforeMethod
+	public void openurl()
+	{
+		driver.get("https://demo.dealsdray.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+	@Test
+	public void test() throws Exception
+	{
+		driver.findElement(By.name("username")).sendKeys("prexo.mis@dealsdray.com");
+		driver.findElement(By.name("password")).sendKeys("prexo.mis@dealsdray.com");
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[2]/div/form/div[3]/div/button")).click();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div[2]/div[1]/div[2]/button")).click();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div[2]/div[1]/div[2]/div/div[1]/a/button")).click();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[2]")).click();
+		driver.findElement(By.xpath("//*[contains(@id,'mui')]")).sendKeys("C:\\Users\\DELL\\Downloads\\demo-data.xlsx");
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/button")).click();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/button")).click();
+		
+		
+		Alert a=driver.switchTo().alert();
+		a.accept();
+
+		
+		File srnshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(srnshot, new File("./screenshot//page1.png"));
+	}
+
+}
